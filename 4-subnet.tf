b8d32f0c421ca361135e0408826f970264cc09ae
unknown <jsw4540@naver.com>
2023-02-06 13:57:31 +0900

    main.tf

--- 4-subnet.tf
+++ 4-subnet.tf
@@ -2,6 +2,6 @@ resource "azurerm_subnet" "web_subnet" {
     name                 = "${var.prefix}-subnet"
     resource_group_name  = azurerm_resource_group.rg.name
     virtual_network_name = azurerm_virtual_network.vnet.name
-    address_prefixs      = ["22.24.10.0/24"]
+    address_prefixs      = ["22.23.10.0/24"]
 }
 
