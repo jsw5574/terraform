485f410c91aa34056b9a08412684f9abb3c5387f
unknown <jsw4540@naver.com>
2023-02-06 14:43:38 +0900

    create subnet

--- variable.tf
+++ variable.tf
@@ -23,7 +23,19 @@ variable "vnet_address_space" {
 
 
 #### 4-subnet address ###
-variable "was_subnet_address" {
+variable "sec_subnet_address" {
+    type = list(string)
+    default = ["22.23.3.0/24"]
+}
+variable "endpoint_subnet_address" {
+    type = list(string)
+    default = ["22.23.5.0/24"]
+}
+variable "vmss_subnet_address" {
+    type = list(string)
+    default = ["22.23.30.0/24"]
+}
+variable "app_subnet_address" {
     type = list(string)
     default = ["22.23.100.0/24"]
 }
