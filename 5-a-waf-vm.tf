2446ae07ffb284d4abcf3bae4ef3dd927c7daac0
unknown <jsw4540@naver.com>
2023-02-06 17:50:29 +0900

    vm create

--- 5-a-waf-vm.tf
+++ 5-a-waf-vm.tf
@@ -1,5 +1,6 @@
 
-/*
+
+### create waf1-vm ##
 resource "azurerm_virtual_machine" "waf1" {
   name                    = "${var.prefix}-waf1-vm"
   location                = var.location
@@ -7,8 +8,8 @@ resource "azurerm_virtual_machine" "waf1" {
   vm_size                 = var.waf_vm_size
   network_interface_ids   = [azurerm_network_interface]
 } 
-*/  
-
+  
+### waf1-nic create ##
 resource "azurerm_network_interface" "waf1_nic" {
   name = "${var.prefix}-waf1-nic"
   location = azurerm_resource_group.rg.location
@@ -30,7 +31,7 @@ locals {
   }
 
 
-
+### waf1-vm + nic + disk attach ##
 resource "azurerm_linux_virtual_machine" "waf1-vm" {
     name = "${var.prefix}-vm"
     resource_group_name = azurerm_resource_group.rg.name
@@ -44,16 +45,17 @@ resource "azurerm_linux_virtual_machine" "waf1-vm" {
       storage_account_type = "Premium_ZRS"
     }
     source_image_reference {
-      publipublisher = "openlogic"
+      publisher = "openlogic"
       offer = "centos"
       sku = "7_9"
       version = "latest"
     }
+    custom_data = base64encode(local.wafvm_custom_data)
 }
 
 
 
-/*
+### create waf2-vm ##
 resource "azurerm_virtual_machine" "waf2" {
   name                    = "${var.prefix}-waf2-vm"
   location                = var.location
@@ -61,8 +63,8 @@ resource "azurerm_virtual_machine" "waf2" {
   vm_size                 = var.waf_vm_size
   network_interface_ids   = [azurerm_network_interface]
 } 
-*/  
-
+  
+### waf2-nic create ##
 resource "azurerm_network_interface" "waf2_nic" {
   name = "${var.prefix}-waf2-nic"
   location = azurerm_resource_group.rg.location
@@ -73,4 +75,35 @@ resource "azurerm_network_interface" "waf2_nic" {
     subnet_id = azurerm_subnet.sec_subnet.id
     private_ip_address_allocation = "Dynamic"
       }
+}
+
+locals {
+  wafvm_custom_data = <<CUSTOM_DATA
+  #!bin/bash
+  sudo timedatectl set-timezone Asia/Seoul
+  
+  CUSTOM_DATA
+  }
+
+
+### waf2-vm + nic + disk attach ##
+resource "azurerm_linux_virtual_machine" "waf2-vm" {
+    name = "${var.prefix}-vm"
+    resource_group_name = azurerm_resource_group.rg.name
+    location = var.location
+    size = "Standard_D2sv5"
+    admin_username = "wjadmin"
+    admin_password = "${var.admin_password}"
+    network_interface_ids = [ azurerm_network_interface.waf2_nic.id ]
+    os_disk {
+      caching = "ReadWrite"
+      storage_account_type = "Premium_ZRS"
+    }
+    source_image_reference {
+      publisher = "openlogic"
+      offer = "centos"
+      sku = "7_9"
+      version = "latest"
+    }
+    custom_data = base64encode(local.wafvm_custom_data)
 }
