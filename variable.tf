95f8e127c3f1e77e0e3cad0d134114642438177e
unknown <jsw4540@naver.com>
2023-02-06 11:45:07 +0900

    variable.tf

---
+++ variable.tf
@@ -0,0 +1,26 @@
+
+
+##### 2-resource-group ####
+
+variable "prefix" {
+  type = string
+  default = "barovone"
+  description = "This is prefix name"
+}
+
+variable "location" {
+    type = string
+    default = "KoreaCentral"
+    dedescription = "This is Location Name"
+}
+
+
+#### 3-virtual-network ###
+variable "address_space" {
+    type = string
+    default = "22.23.0.0/16"
+}
+
+variable "subnet_names" {
+  
+}
