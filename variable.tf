92a9b86d4deb8441ebc4bd6c49776dfe5553fae7
unknown <jsw4540@naver.com>
2023-02-06 13:43:04 +0900

    main.tf

--- variable.tf
+++ variable.tf
@@ -20,3 +20,10 @@ variable "vnet_address_space" {
     type = list(string)
     default = ["22.23.0.0/16"]
 }
+
+
+#### 4-subnet address ###
+variable "web_subnet_address" {
+    type = list(string)
+    default = ["22.23.100.0/24"]
+}
