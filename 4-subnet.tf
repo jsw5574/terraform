81a6b15177d4fb7ab1cd4c5c9e15ff2a4429be0d
unknown <jsw4540@naver.com>
2023-02-06 13:47:25 +0900

    main.tf

--- 4-subnet.tf
+++ 4-subnet.tf
@@ -5,35 +5,3 @@ resource "azurerm_subnet" "web_subnet" {
     address_prefixs      = "${var.web_subnet_address}"  
 }
 
-resource "azurerm_network_security_group" "web_subnet_nsg" {
-    name                = "${azurerm_subnet.web_subnet.name}-nsg"
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_subnet_network_security_group_association" "web_subnet_nsg_associate" {
-    depends_on = [ azurerm_network_security_rule.web_nsg_rule_inbound ]
-    subnet_id   = azurerm_network_security_group.web_subnet_nsg.id
-}
-
-locals {
-  web_inbound_ports_map = {
-    "100" : "80",
-    "110" : "443",
-  }
-}
-
-resource "azurerm_network_security_rule" "web_nsg_rule_inbound" {
-    for_each = local.web_inbound_ports_map
-    name                         = "Allow-${each.value}"
-    priority                     = each.key
-    direction                    = "Inbound"
-    access                       = "Allow"
-    protocol                     = "Tcp"
-    source_port_range            = "*"
-    destination_port_range       = each.value
-    source_address_prefix        = "*"
-    destination_address_prefix   = "*"
-    resource_group_name          = azurerm_resource_group.rg.name
-    network_security_group_name  = azurerm_network_security_group.web_subnet_nsg.name
-      }   
