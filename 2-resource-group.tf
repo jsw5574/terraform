5bc6c1671cbe5f1658e3a2a9b32bac896b58feab
unknown <jsw4540@naver.com>
2023-02-06 13:22:52 +0900

    main.tf

--- 2-resource-group.tf
+++ 2-resource-group.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "rg" {
-  name = "${var.prefix}"-rg
+  name = "${var.prefix}-rg"
   location = "${var.location}"
 }
--- 3-virtual-network.tf
+++ 3-virtual-network.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "vnet" {
-    name = "${var.prefix}"-vnet
+    name = "${var.prefix}-vnet"
     resource_group_name = azurerm_resource_group.rg
     location = azurerm_resource_group.rg
     address_space = "${var.address_space}"
