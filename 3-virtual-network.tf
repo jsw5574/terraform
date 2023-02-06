097640c9a08e2a7acde591349f1bc3c09e7f9284
jsw5574 <50002417+jsw5574@users.noreply.github.com>
2023-02-06 10:57:10 +0900

    Create 3-virtual-network.tf

---
+++ 3-virtual-network.tf
@@ -0,0 +1,7 @@
+resource "azurerm_virtual_network" "vnet" {
+    name = "${var.prefix}"-vnet
+    resource_group_name = azurerm_resource_group.rg
+    location = azurerm_resource_group.rg
+    address_space = "${var.address_space}"
+}
+
